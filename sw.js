f399cd29121230c1ec527aca518e6155fa0ee8c0
Matt Riggott <MRiggott@comarktv.com>
2019-05-02 12:55:50 -0400

    add sw install, update sw with scope + adjust cached filepaths work with github pages

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-var cacheName = 'pwa-playground';
+var APP_NAME = 'pwa-playground_';
+var VERSION = 'version_1';
+var cacheName = APP_NAME + VERSION;
+var repoPath = 'pwa-playground';
 var filesToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js'
 ];
+ 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+      let cacheFilePaths = filesToCache.map(f => `/${repo}${f}`);
+      return cache.addAll(cacheFilePaths);
     })
   );
 });
